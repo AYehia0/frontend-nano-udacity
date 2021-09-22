f74f9c78702e8e40fe0370a2f26373db99445254
AYehia0 <ahmedyehia611@gmail.com>
2021-09-22 23:49:08 +0200

    all the logic is done :D , there is a bug, can you find it out ?

--- js/app.js
+++ js/app.js
@@ -19,6 +19,9 @@
  * 
 */
 
+// getting all the nav links 
+const navSectionLinks = document.querySelectorAll('.menu__link')
+const sections = document.querySelectorAll('section')
 
 /**
  * End Global Variables
@@ -26,7 +29,10 @@
  * 
 */
 
-
+// get a section id from the menu__link li element
+const generateSectionId = (menuElement) => {
+    return menuElement.id.replace('s', 'section')
+}
 
 /**
  * End Helper Functions
@@ -34,24 +40,99 @@
  * 
 */
 
-// build the nav
+// make sure there is only one active link at a time
+const ensureActiveStatus = (elemntList, activeClass) => {
+
+    // getting all menu links classes
+     elemntList.forEach(el => {
+
+        const status = el.classList
+        if (status.contains(activeClass)) {
+            // remove it 
+            status.remove(activeClass)
+        }
+    })
+}
+
+// Scroll to a section by its id
+const scrollToSectionById = (sectionId) => {
+
+    // scrolling options
+    const scrollOpts = {
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest"
+    }
+
+    // getting the element by its id
+    const section = document.querySelector(`#${sectionId}`)
+
+    // check if section doesn't exist
+    if (!section)
+        return null
+
+    // scrolling to it using scrollIntoView : https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
+    section.scrollIntoView(scrollOpts)
+}
+
+// observer options 
+const observerOpts = {
+    root: null, // it's in the viewport
+    threshold: 0.7, // the thershold decides 70% of the content are in the spot to fire
+    //rootMargin: "-200px"
+}
+
+// Scrolling observer
+const sectionObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+
+        // checking for itersecting
+        // to prevent firing when page is opened the first time
+        if (!entry.isIntersecting){
+            return
+        }
+
+        // checking the id
+        const entryId = entry.target.id
+
+        console.log(entryId)
+
+        // change the nav status to active 
+        ensureActiveStatus(navSectionLinks, "active")
+        ensureActiveStatus(sections, "your-active-class")
+
+        // getting the li element linked to that section 
+        // add active  
+        document.querySelector(`#${entryId.replace('section', 's')}`).classList.toggle('active')
+        document.querySelector(`#${entryId}`).classList.add('your-active-class')
+       
+    })
+
+}, observerOpts)
 
 
-// Add class 'active' to section when near top of viewport
+navSectionLinks.forEach(navSection => {
+    navSection.addEventListener('click', () => {
+
+        // getting the section id it suppose to go to 
+        const sectionIdToGo = generateSectionId(navSection)
+
+        // toggling active in menu
+        navSection.classList.toggle('active')
+
+        // toggling active in section
+        document.querySelector(`#${sectionIdToGo}`).classList.add('your-active-class')
 
 
-// Scroll to anchor ID using scrollTO event
+        //console.log(`clicked: ${navSection.id}, ${sectionIdToGo}`)
 
+        // scroll to the section 
+        scrollToSectionById(sectionIdToGo)
 
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
+    })
+})
 
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
+// observer thing
+sections.forEach(section => {
+    sectionObserver.observe(section)
+})
